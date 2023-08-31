76728dc5ba8cd84121ab8d103921924259424f60
Aakash Dev <100058578+aakash07l@users.noreply.github.com>
2023-08-31 10:16:38 +0530

    Update Header.js

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -10,3 +10,27 @@ export default Header
 
 import Logo from "../moralis-logo.svg";
 import Eth from "../eth.svg";
+
+function Header(props) {
+
+  const {address, isConnected, connect} = props;
+
+  return (
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <div className="headerItem">Swap</div>
+        <div className="headerItem">Tokens</div>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        <div className="connectButton" onClick={connect}>
+          {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
+        </div>
+      </div>
+    </header>
+  );
+}
